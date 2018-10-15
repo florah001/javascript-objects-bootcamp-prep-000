bd2c8c2f612fa67a731cd9797faa68a9d22ae30c
florah001 <florah001@yahoo.co.uk>
2018-10-15 06:07:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,8 +4,9 @@ var playlist = { artistnames: 'song titles'
       playlist.songTitle  = 'My Bloody Valentine';
       playlist.artistName = 'Phil Ochs';
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {'artistName': 'songTitle`})
+function updatePlaylist(playlist, artistName, songTitle){
+        playlist[artistName] = songTitle;
+        return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
