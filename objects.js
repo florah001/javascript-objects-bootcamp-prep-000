1ff2f157b5ce2ea639d2efc0b98006edf2df042f
florah001 <florah001@yahoo.co.uk>
2018-10-15 05:48:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -14,23 +14,3 @@ function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
 }
 
-/*
-function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({}, { playlist: 'Phil Ochs' });
-  return 'Slowdive', 'My Bloody Valentine', and 'Phil Ochs';
-}
-
-
-
-
-}
-
-function updatePlaylist(playlist, artistName, songTitle) {
-   Object.assign({}, 'Slowdive', { ['Phil Ochs']: 'My Bloody Valentine'});
-   return playlist;
-AssertionError: expected { Object (artistnames, Slowdive, ...) } to contain keys 'Slowdive', 'My Bloody Valentine', and 'Phil Ochs'
-
-objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
-        AssertionError: expected { Object (artistnames, Slowdive, ...) } to contain keys 'Slowdive', 'My Bloody Valentine', and 'Phil Ochs'
-      + expected - actual
-*/
