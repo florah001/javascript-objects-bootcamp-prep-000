86738a1f7371dfac0e12832307902ef49c7cc74a
florah001 <florah001@yahoo.co.uk>
2018-10-15 05:03:24 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = { artistnames: 'song titles' 
+};
+      playlist = Slowdive;
+      playlist.songTitle  = 'My Bloody Valentine';
+      playlist.artistName = 'Phil Ochs';
+function updatePlaylist(playlist, artistName, songTitle) {
+   Object.assign({}, 'Slowdive', { ['Phil Ochs']: 'My Bloody Valentine'});
+   return playlist;
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+}
+
+/*
