2745043dfb37aaf14923cd1e8a4ca2ddae4d6eac
florah001 <florah001@yahoo.co.uk>
2018-10-15 05:32:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,16 +3,27 @@ var playlist = { artistnames: 'song titles'
       playlist = Slowdive;
       playlist.songTitle  = 'My Bloody Valentine';
       playlist.artistName = 'Phil Ochs';
-function updatePlaylist(playlist, artistName, songTitle) {
-   Object.assign({}, 'Slowdive', { ['Phil Ochs']: 'My Bloody Valentine'});
-   return playlist;
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = 'Phil Ochs'
+ 
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
 }
 
-/*objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
+/*
+
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+   Object.assign({}, 'Slowdive', { ['Phil Ochs']: 'My Bloody Valentine'});
+   return playlist;
+
+
+objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
         AssertionError: expected { Object (artistnames, Slowdive, ...) } to contain keys 'Slowdive', 'My Bloody Valentine', and 'Phil Ochs'
       + expected - actual
 */
